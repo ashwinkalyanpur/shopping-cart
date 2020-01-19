09e5226575f70b83f30e39bf8150e8214c5256a7
ashwinkalyanpur <akalyanpur1974@gmail.com>
2020-01-20 02:03:37 +0530

    Added Shopping cart feature

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,11 +7,16 @@ import { ShoppingRecipe } from '../shopping-list/shopping-recipe';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  public counter : number = 0; 
   @Output() recipeWasSelected = new EventEmitter<ShoppingRecipe>();
   constructor() { }
 
   ngOnInit() {
   }
+
+  increment(){this.counter += 1;}
+  decrement(){if(this.counter >1){this.counter -= 1;}}
+
   onRecipeSelected(items: ShoppingRecipe){
     this.recipeWasSelected.emit(items);
   }
